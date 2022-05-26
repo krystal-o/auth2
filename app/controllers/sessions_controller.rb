e03eaea30e502dfea4caea5e19b3e127eb512cb5
krystal-o <krystal.onyema@kellogg.northwestern.edu>
2022-05-26 22:46:54 +0000

    saving spot

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+        @user = User.find_by({"email"=> params["email"]})
+        if @user
+            if @user["password"] == params["password"]
+                # login the user
+                flash["notice"] = "You've logged in."
+                redirect_to "/companies"
+            else
+                flash["notice"] = "Unsuccessful login."
+                redirect_to "/sessions/new"
+            end   
+        else
+            flash["notice"] = "Unsuccessful login"
+            redirect_to "/sessions/new"       
+        end
+    end
+end
+
